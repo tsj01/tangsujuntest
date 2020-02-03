1c6e742b11660edf2a00df5186374f69f6b32dfc
tangsujun <981526250@qq.com>
2020-02-03 19:37:10 +0800

    扫码图片上传

--- pages/newOrder/index.js
+++ pages/newOrder/index.js
@@ -8,7 +8,51 @@ Page({
     show: false,
     array: ['是', '否'],
     index:1,
-    time:''
+    time:'1212',
+    date: '2019-01-01 13:37',
+    disabled: false,//设置是否能点击 false可以 true不能点击
+    startDate: '2019-01-01 12:37',
+    endDate: '2050-03-12 12:38',
+    placeholder: '请选择时间',
+    orderList: [
+      { name: '配件吗', num: 1212 }, 
+      { name: '配件吗', num: 1212 },
+      { name: '配件吗', num: 1212 },
+      { name: '配件吗', num: 1212 },
+      { name: '配件吗', num: 1212 },
+      { name: '配件吗', num: 1212 }
+      ],
+    fileList: [{ url: 'https://img.yzcdn.cn/vant/leaf.jpg', name: '图片1' },]
+  },
+  deleteimg(e){
+    console.log(e)
+  },
+  afterRead(event) {
+    const { file } = event.detail;
+    // 当设置 mutiple 为 true 时, file 为数组格式，否则为对象格式
+    wx.uploadFile({
+      url: 'https://example.weixin.qq.com/upload', // 仅为示例，非真实的接口地址
+      filePath: file.path,
+      name: 'file',
+      formData: { user: 'test' },
+      success(res) {
+        // 上传完成需要更新 fileList
+        const { fileList = [] } = this.data;
+        fileList.push({ ...file, url: res.data });
+        this.setData({ fileList });
+      }
+    });
+  },
+  edit:function (e){
+    let query = e.currentTarget.dataset['index'];
+    console.log(query,1111)
+    this.setData({ show: true });
+  },
+  onPickerChange: function (e) {
+    console.log(e.detail);
+    this.setData({
+      date: e.detail.dateString
+    })
   },
   showPopup() {
     this.setData({ show: true });
@@ -26,11 +70,43 @@ Page({
       index: e.detail.value
     })
   },
+  getScancode: function () {
+    var _this = this;
+    // 允许从相机和相册扫码
+    wx.scanCode({
+      success: (res) => {
+        var result = res.result;
+        var scanType = res.scanType;
+        var charSet = res.charSet;
+        var path = res.path;
+        _this.setData({
+          result: result,
+          scanType: scanType,
+          charSet: charSet,
+          path: path
+        })
+      }
+    })
+    // 只允许从相机扫码
+    // wx.scanCode({
+    //   onlyFromCamera: true,
+    //   success: (res) => {
+    //     console.log(res)
+    //   }
+    // })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    let that = this;
+    setTimeout(function () {
+      console.log('doSomething')
+      that.setData({
+        date: '2019-01-01 13:37',
+        placeholder: '2019-01-01 13:37'
+      })
+    }, 2000);
   },
 
   /**
