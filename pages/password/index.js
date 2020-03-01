303007125c6b551bca5f925897d3dd2c2a6809e7
tangsujun <981526250@qq.com>
2020-03-01 19:46:09 +0800

    updat

--- pages/password/index.js
+++ pages/password/index.js
@@ -1,19 +1,76 @@
+var md5 = require('../../utils/md5.js');
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    oldpwd:'',
+    newpwd:'',
+    setpwd:''
   },
-
+  oldpwdChange:function(e){
+    this.setData({
+      oldpwd: e.detail
+    })
+  },
+  newpwdChange: function (e) {
+    console.log(e)
+    this.setData({
+      newpwd: e.detail
+    })
+  },
+  setpwdChange: function (e) {
+    this.setData({
+      setpwd: e.detail
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    console.log(this.data.oldpwd)
   },
-
+  submit() {
+    var that = this;
+    console.log(11111)
+    console.log(that.data.oldpwd);
+    if (that.data.newpwd !== that.data.setpwd) {
+      wx.showToast({
+        title: '新密码与确认密码不一样',
+        icon: 'none',
+      })
+      return;
+    }
+    wx.request({
+      url: 'http://kld.8866.org:8088/dingdong/mobile/doAction?method=resetpwd',
+      method: 'POST',
+      data: {
+        old_pwd: md5.hexMD5(that.data.oldpwd),
+        new_pwd: md5.hexMD5(that.data.newpwd),
+        usePaging: false,
+        uid_: '',
+        kldkey: '5633838366032366735303566353562626169353162693439333364616031356323333237393632373335313',
+        openid: '',
+        nickname: '',
+        ver: 200
+      },
+      header: {
+        'content-type': 'application/x-www-form-urlencoded' //修改此处即可
+      },
+      success: function (res) {
+        if (res.statusCode == 200) {
+          wx.showToast({
+            title: res.data.message,
+          })
+        } else {
+          wx.showToast({
+            title: res.data.message,
+          })
+        }
+      }
+    })
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
