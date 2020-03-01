d071684c48decd30921dc8d0b94d8dfcdb21ba90
tswsoft <tswsoft@163.com>
2020-03-01 18:36:42 +0800

    no message

--- app.js
+++ app.js
@@ -37,7 +37,8 @@ App({
   globalData: {
     userInfo: null,
     serverUrl: 'http://kld.8866.org:8088/dingdong/mobile',
-    lastMonths: -2
+    lastMonths: -2,
+    attrUrl: 'http://kld.8866.org:8088/dingdong/static/static/upload/'
   },
   sendRequest: function(options) {
     var self = this;
